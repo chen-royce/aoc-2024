5764279ff9be4428fce9b7e04cc9dcecf43cb2c9
Royce Chen <royce.chen.y@gmail.com>
2024-12-01 02:18:01 -0500

    day 2 second star

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
@@ -11,8 +12,11 @@ fn main() {
         let list_two: Vec<i64> = get_nth_elements_from_inputs(&data, 1);
 
         let sum: i64 = part_1(&list_one, &list_two);
+        let similarity_score: i64 = part_2(&list_one, &list_two);
 
-        println!("{}", sum);
+        println!("{} part 1: {}", path, sum);
+        println!("{} part 2: {}", path, similarity_score);
+        println!("");
     }
 }
 
@@ -23,6 +27,23 @@ fn part_1(list_one: &Vec<i64>, list_two: &Vec<i64>) -> i64 {
         .map(|(l, r)| (l - r).abs())
         .sum()
 }
+
+fn part_2(list_one: &Vec<i64>, list_two: &Vec<i64>) -> i64 {
+    let freqs = vec_to_freq_map(list_two);
+    list_one
+        .iter()
+        .filter_map(|&num| freqs.get(&num).map(|&freq| num * freq as i64))
+        .sum()
+}
+
+fn vec_to_freq_map(vec: &Vec<i64>) -> HashMap<i64, usize> {
+    let mut freq_map = HashMap::new();
+    for num in vec {
+        *freq_map.entry(*num).or_insert(0) += 1;
+    }
+    freq_map
+}
+
 fn get_nth_elements_from_inputs(data: &String, n: usize) -> Vec<i64> {
     data.split('\n')
         .filter_map(|line| line.split("   ").nth(n)?.parse::<i64>().ok())
