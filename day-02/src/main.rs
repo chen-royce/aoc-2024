0b0ed1e8c413429cbe274be549c407469d83fe48
Royce Chen <royce.chen.y@gmail.com>
2024-12-02 01:45:24 -0500

    day 2 second star, messy but it's late

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,5 +1,5 @@
 use core::num;
-use std::fs;
+use std::{fs, usize};
 
 fn main() {
     let paths = ["inputs/example.txt", "inputs/data.txt"];
@@ -16,41 +16,59 @@ fn main() {
                 .split(' ')
                 .filter_map(|num| num.parse::<i64>().ok())
                 .collect();
-
-            let mut movement_started = false;
-            let mut last_difference = 0;
-
-            for (idx, elem) in levels[1..].iter().enumerate() {
-                println!("{}", elem);
-                let curr_difference = elem - levels[idx];
-
-                if curr_difference == 0 {
-                    println!("Gotta keep moving");
-                    num_safe_reports -= 1;
-                    break;
-                }
-
-                if curr_difference.abs() > 3 {
-                    println!("Too large of a movement");
-                    num_safe_reports -= 1;
-                    break;
-                }
-
-                if !movement_started {
-                    last_difference = curr_difference;
-                    movement_started = true;
-                }
-
-                if (last_difference > 0 && curr_difference < 0)
-                    || (last_difference < 0 && curr_difference > 0)
-                {
-                    println!("Switched signs");
-                    num_safe_reports -= 1;
-                    break;
-                }
+            let permutations = versions_with_one_element_removed(levels);
+            if !permutations
+                .iter()
+                .map(|p| report_is_safe(p))
+                .any(|result| result)
+            {
+                num_safe_reports -= 1;
             }
         }
 
         println!("Number of safe reports: {}", num_safe_reports);
     }
+}
+
+fn versions_with_one_element_removed(vec: Vec<i64>) -> Vec<Vec<i64>> {
+    let mut result = Vec::new();
+    for i in 0..vec.len() {
+        let mut new_vec = vec.clone();
+        new_vec.remove(i);
+        result.push(new_vec);
+    }
+    result
+}
+
+fn report_is_safe(levels: &Vec<i64>) -> bool {
+    let mut movement_started = false;
+    let mut last_difference = 0;
+    for (idx, elem) in levels[1..].iter().enumerate() {
+        println!("{}", elem);
+        let curr_difference = elem - levels[idx];
+
+        if curr_difference == 0 {
+            println!("Gotta keep moving");
+            return false;
+        }
+
+        if curr_difference.abs() > 3 {
+            println!("Too large of a movement");
+            return false;
+        }
+
+        if !movement_started {
+            last_difference = curr_difference;
+            movement_started = true;
+        }
+
+        if (last_difference > 0 && curr_difference < 0)
+            || (last_difference < 0 && curr_difference > 0)
+        {
+            println!("Switched signs");
+            return false;
+        }
+    }
+
+    return true;
 }
