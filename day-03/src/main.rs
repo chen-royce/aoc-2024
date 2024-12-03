2f1c657a0db3d3f7f56b53330fd1d9ecd4119960
Royce Chen <royce.chen.y@gmail.com>
2024-12-03 01:32:37 -0500

    day 3 first star, fix corner case

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,7 +1,11 @@
 use std::fs;
 
 fn main() {
-    let paths = ["inputs/example.txt", "inputs/data.txt"];
+    let paths = [
+        "inputs/example.txt",
+        "inputs/data.txt",
+        "inputs/corner-case.txt",
+    ];
 
     for path in paths {
         let mut curr_total = 0;
@@ -10,23 +14,24 @@ fn main() {
         let equations: Vec<&str> = data.split("mul(").collect();
 
         for eq in &equations[1..] {
-            count += 1;
-            println!("eq #{}: {}", count, eq);
             let factors: Vec<&str> = eq.split(',').collect();
             if factors.len() < 2 {
                 continue;
             }
             let first_factor = factors[0].parse::<u128>().ok();
-            let second_factor_string = factors[1].split(')').next();
 
-            let mut second_factor: Option<u128> = None;
-            match second_factor_string {
-                Some(s) => second_factor = s.parse::<u128>().ok(),
-                _ => continue,
+            let closing_paren_index = factors[1].find(')');
+            let second_factor_string = match closing_paren_index {
+                Some(index) => &factors[1][..index],
+                None => continue,
             };
+
+            let second_factor = second_factor_string.parse::<u128>().ok();
 
             match (first_factor, second_factor) {
                 (Some(num1), Some(num2)) => {
+                    count += 1;
+                    println!("eq #{}: {}", count, eq);
                     let product = num1 * num2;
                     println!("{}", product);
                     println!("");
